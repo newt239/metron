341049a08dbd8ca1e2b79fb02bee3053ba1c1f9d
newt239 <ysti2019.win2020.st@gmail.com>
2023-06-18 14:53:12 +0900

    improve: styles

--- types/spotify.ts
+++ types/spotify.ts
@@ -1,157 +1,271 @@
 export interface ProfileProps {
-  id: string
-  display_name: string
-  images: { url: string }[]
-  country: string
-  type: "user"
+  id: string;
+  display_name: string;
+  images: { url: string }[];
+  country: string;
+  type: "user";
 }
 
 export interface TrackProps {
-  album: Album
-  artists: DetailedArtist[]
-  available_markets: string[]
-  disc_number: number
-  duration_ms: number
-  explicit: boolean
-  external_ids: ExternalIds
-  external_urls: ExternalUrls
-  href: string
-  id: string
-  is_playable: boolean
-  linked_from: LinkedFrom
-  restrictions: Restrictions
-  name: string
-  popularity: number
-  preview_url?: string
-  track_number: number
-  type: string
-  uri: string
-  is_local: boolean
+  album: Album;
+  artists: DetailedArtist[];
+  available_markets: string[];
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_ids: ExternalIds;
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  is_playable: boolean;
+  linked_from: LinkedFrom;
+  restrictions: Restrictions;
+  name: string;
+  popularity: number;
+  preview_url?: string;
+  track_number: number;
+  type: string;
+  uri: string;
+  is_local: boolean;
 }
 
 export interface AudioInfoProps {
-  acousticness: number
-  analysis_url: string
-  danceability: number
-  duration_ms: number
-  energy: number
-  id: string
-  instrumentalness: number
-  key: number
-  liveness: number
-  loudness: number
-  mode: number
-  speechiness: number
-  tempo: number
-  time_signature: number
-  track_href: string
-  type: string
-  uri: string
-  valence: number
+  acousticness: number;
+  analysis_url: string;
+  danceability: number;
+  duration_ms: number;
+  energy: number;
+  id: string;
+  instrumentalness: number;
+  key: number;
+  liveness: number;
+  loudness: number;
+  mode: number;
+  speechiness: number;
+  tempo: number;
+  time_signature: number;
+  track_href: string;
+  type: string;
+  uri: string;
+  valence: number;
 }
 
 export interface ArtistProps {
-  external_urls: ExternalUrls
-  followers: Followers
-  genres: string[]
-  href: string
-  id: string
-  images: ImagesEntity[]
-  name: string
-  popularity: number
-  type: string
-  uri: string
+  external_urls: ExternalUrls;
+  followers: Followers;
+  genres: string[];
+  href: string;
+  id: string;
+  images: ImagesEntity[];
+  name: string;
+  popularity: number;
+  type: string;
+  uri: string;
 }
 
 export interface ImagesEntity {
-  url: string
-  height: number
-  width: number
+  url: string;
+  height: number;
+  width: number;
 }
 
 export interface ExternalUrls {
-  spotify: string
+  spotify: string;
 }
 
 export interface Image {
-  url: string
-  height: number
-  width: number
+  url: string;
+  height: number;
+  width: number;
 }
 
 export interface Restrictions {
-  reason: string
+  reason: string;
 }
 
 export interface Artist {
-  external_urls: ExternalUrls
-  href: string
-  id: string
-  name: string
-  type: string
-  uri: string
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  name: string;
+  type: string;
+  uri: string;
 }
 
 export interface Album {
-  album_type: string
-  total_tracks: number
-  available_markets: string[]
-  external_urls: ExternalUrls
-  href: string
-  id: string
-  images: Image[]
-  name: string
-  release_date: string
-  release_date_precision: string
-  restrictions: Restrictions
-  type: string
-  uri: string
-  album_group: string
-  artists: Artist[]
+  album_type: string;
+  total_tracks: number;
+  available_markets: string[];
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  images: Image[];
+  name: string;
+  release_date: string;
+  release_date_precision: string;
+  restrictions: Restrictions;
+  type: string;
+  uri: string;
+  album_group: string;
+  artists: Artist[];
 }
 export interface Followers {
-  href: string
-  total: number
+  href: string;
+  total: number;
 }
 
 export interface DetailedArtist {
-  external_urls: ExternalUrls
-  followers: Followers
-  genres: string[]
-  href: string
-  id: string
-  images: Image[]
-  name: string
-  popularity: number
-  type: string
-  uri: string
+  external_urls: ExternalUrls;
+  followers: Followers;
+  genres: string[];
+  href: string;
+  id: string;
+  images: Image[];
+  name: string;
+  popularity: number;
+  type: string;
+  uri: string;
 }
 
 export interface ExternalIds {
-  isrc: string
-  ean: string
-  upc: string
+  isrc: string;
+  ean: string;
+  upc: string;
 }
 
 export interface LinkedFrom {
-  album: Album
-  artists: Artist[]
-  available_markets: string[]
-  disc_number: number
-  duration_ms: number
-  explicit: boolean
-  external_ids: ExternalIds
-  external_urls: ExternalUrls
-  href: string
-  id: string
-  is_playable: boolean
-  linked_from: LinkedFrom
-  restrictions: Restrictions
-  name: string
-  popularity: number
-  preview_url: string
-  track_number: number
-  type: string
-  uri: string
-  is_local: boolean
+  album: Album;
+  artists: Artist[];
+  available_markets: string[];
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_ids: ExternalIds;
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  is_playable: boolean;
+  linked_from: LinkedFrom;
+  restrictions: Restrictions;
+  name: string;
+  popularity: number;
+  preview_url: string;
+  track_number: number;
+  type: string;
+  uri: string;
+  is_local: boolean;
+}
+
+export interface PlaybackState {
+  device: Device;
+  repeat_state: string;
+  shuffle_state: boolean;
+  context: Context;
+  timestamp: number;
+  progress_ms: number;
+  is_playing: boolean;
+  item: Item;
+  currently_playing_type: string;
+  actions: Actions;
+}
+
+export interface Device {
+  id: string;
+  is_active: boolean;
+  is_private_session: boolean;
+  is_restricted: boolean;
+  name: string;
+  type: string;
+  volume_percent: number;
+}
+
+export interface Context {
+  type: string;
+  href: string;
+  external_urls: ExternalUrls;
+  uri: string;
+}
+
+export interface Item {
+  album: Album;
+  artists: Artist2[];
+  available_markets: string[];
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_ids: ExternalIds2;
+  external_urls: ExternalUrls5;
+  href: string;
+  id: string;
+  is_playable: boolean;
+  linked_from: LinkedFrom;
+  restrictions: Restrictions2;
+  name: string;
+  popularity: number;
+  preview_url: string;
+  track_number: number;
+  type: string;
+  uri: string;
+  is_local: boolean;
+}
+
+export interface ExternalUrls2 {
+  spotify: string;
+}
+
+export interface Copyright {
+  text: string;
+  type: string;
+}
+export interface ExternalUrls3 {
+  spotify: string;
+}
+
+export interface Artist2 {
+  external_urls: ExternalUrls4;
+  followers: Followers;
+  genres: string[];
+  href: string;
+  id: string;
+  images: Image2[];
+  name: string;
+  popularity: number;
+  type: string;
+  uri: string;
+}
+
+export interface ExternalUrls4 {
+  spotify: string;
+}
+
+export interface Image2 {
+  url: string;
+  height: number;
+  width: number;
+}
+
+export interface ExternalIds2 {
+  isrc: string;
+  ean: string;
+  upc: string;
+}
+
+export interface ExternalUrls5 {
+  spotify: string;
+}
+export interface Restrictions2 {
+  reason: string;
+}
+
+export interface Actions {
+  interrupting_playback: boolean;
+  pausing: boolean;
+  resuming: boolean;
+  seeking: boolean;
+  skipping_next: boolean;
+  skipping_prev: boolean;
+  toggling_repeat_context: boolean;
+  toggling_shuffle: boolean;
+  toggling_repeat_track: boolean;
+  transferring_playback: boolean;
 }
