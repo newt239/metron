4896afc78e3b1ce68ccf4700b4a52462a4b6b769
newt239 <ysti2019.win2020.st@gmail.com>
2023-06-18 11:48:42 +0900

    refactor: re setup with shadcn/ui templete

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['i.scdn.co'],
+  experimental: {
+    appDir: true,
   },
 }
 
-module.exports = nextConfig
+module.export = nextConfig
